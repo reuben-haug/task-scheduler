08eaf26632e167b292a9846c6c86085bb5628869
reuben-haug <reuben.haug@gmail.com>
2023-12-02 11:58:11 -0800

    Added setup.py to troubleshoot failing imports from test files

--- scheduler/database.py
+++ scheduler/database.py
@@ -1,45 +1,48 @@
 import sqlite3
 
-def setup_database():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('schedule.db')
+class Database:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
 
-    # Create a cursor object
-    c = conn.cursor()
+    def setup(self):
+        # Create table for Technicians if it doesn't exist
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Technicians(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                availability INTEGER
+            )
+        ''')
 
-    # Create table for Technicians if it doesn't exist
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS Technicians(
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            availability INTEGER
-        )
-    ''')
+        # Create table for Sites if it doesn't exist
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Sites(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                address TEXT,
+                devices TEXT
+            )
+        ''')
 
-    # Create table for Sites if it doesn't exist
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS Sites(
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            address TEXT,
-            devices TEXT
-        )
-    ''')
+        # Create table for Tasks if it doesn't exist
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Tasks(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                duration INTEGER,
+                site_id INTEGER,
+                FOREIGN KEY(site_id) REFERENCES Sites(id)
+            )
+        ''')
 
-    # Create table for Tasks if it doesn't exist
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS Tasks(
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            duration INTEGER,
-            site_id INTEGER,
-            FOREIGN KEY(site_id) REFERENCES Sites(id)
-        )
-    ''')
+        # Commit the changes
+        self.conn.commit()
 
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
+    def close(self):
+        self.conn.close()
 
 if __name__ == "__main__":
-    setup_database()
+    db = Database('schedule.db')
+    db.setup()
+    db.close()
