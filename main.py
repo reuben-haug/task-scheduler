d746d2fa98e345130f611d3f29b264af4003379a
reuben-haug <reuben.haug@gmail.com>
2023-12-02 10:43:08 -0800

    Added comments to main.py, created README.md and CHANGELOG.md files

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import sqlite3
+
+# Database class for handling SQLite operations
 class Database:
     def __init__(self, db_name):
+        # Connect to the SQLite database
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
 
     def fetch_by_id(self, table, id):
+        # Fetch a row from a table by id
         self.cursor.execute(f'SELECT * FROM {table} WHERE id=?', (id,))
         return self.cursor.fetchone()
 
     def close(self):
+        # Close the database connection
         self.conn.close()
 
+# Technician class for handling technician data and operations
 class Technician:
     def __init__(self, id, db):
+        # Fetch technician data from the database
         row = db.fetch_by_id('Technicians', id)
         self.id = row[0]
         self.name = row[1]
@@ -19,6 +27,7 @@ class Technician:
         self.tasks = []
 
     def assign_task(self, task):
+        # Assign a task to the technician if they have enough availability
         if self.availability >= task.duration:
             self.tasks.append(task)
             self.availability -= task.duration
@@ -26,22 +35,27 @@ class Technician:
         else:
             return False
 
+# Task class for handling task data and operations
 class Task:
     def __init__(self, id, db):
+        # Fetch task data from the database
         row = db.fetch_by_id('Tasks', id)
         self.id = row[0]
         self.name = row[1]
         self.duration = row[2]
         self.site = Site(row[3], db)
 
+# Site class for handling site data and operations
 class Site:
     def __init__(self, id, db):
+        # Fetch site data from the database
         row = db.fetch_by_id('Sites', id)
         self.id = row[0]
         self.name = row[1]
         self.address = row[2]
         self.devices = row[3]
 
+# Function to schedule tasks to technicians
 def schedule_tasks(technicians, tasks, db):
     for task in tasks:
         for tech in technicians:
